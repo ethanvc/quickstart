3708c791c8be6ac4a063ae205df4cf314c5afc73
ethanvc <xht_xht@qq.com>
2023-05-16 14:10:06 +0800

    add code

--- redis/a1_timeout/main.go
+++ redis/a1_timeout/main.go
@@ -8,19 +8,20 @@ import (
 )
 
 // trigger connection pool timeout error.
+// docker run --rm -it -p 10000:6379 redis
 func main() {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     "3.3.3.3:10000",
 		PoolSize: 1,
 	})
-	for i := 0; i < 3; i++ {
+	for i := 0; i < 1; i++ {
 		go get(rdb, "0")
 	}
 	time.Sleep(10000 * time.Hour)
 }
 
 func get(rdb *redis.Client, key string) {
-	c, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	c, cancel := context.WithTimeout(context.Background(), 10000*time.Second)
 	defer cancel()
 	v := rdb.Get(c, key)
 	fmt.Println(v)
