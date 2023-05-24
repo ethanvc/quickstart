884f2cb893ab948385bdddab92f07dd6612057dd
ethanvc <xht_xht@qq.com>
2023-05-24 21:08:49 +0800

    add code

--- langchain/a0_demo/main.py
+++ langchain/a0_demo/main.py
@@ -1,5 +1,6 @@
 from langchain.llms import OpenAI
 
+# please use python 3.11 to run it, 3.9 may arise error.
 llm = OpenAI(temperature=0.9)
 text = "What would be a good company name for chat service?"
 resp = llm(text)
