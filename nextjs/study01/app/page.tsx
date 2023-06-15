d1d3ab6fb4c42b8d48ab5074cc526456dbbe798d
ethanvc <xht_xht@qq.com>
2023-06-15 23:16:57 +0800

    add code

--- nextjs/study01/app/page.tsx
+++ nextjs/study01/app/page.tsx
@@ -1,22 +1,26 @@
 import Image from 'next/image'
 
 export default function Home() {
+    const items = ["不限提问次数", "有效期：7天", "字数：45000个", "支持上下文关联", "直连模式"];
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div>
             <div className="w-[280px] border-[1px] border-[rgb(103,194,58)] p-[10px]">
-                <div className="text-[rgb(24, 160, 88)]">体验套餐</div>
-                <div>¥9.90</div>
-                <div>原价：19.90</div>
-                <div>
-                    <ul>
-                        <li>有没的校园环境</li>
-                        <li>资深的教学团队</li>
-                        <li>额外的水果套餐</li>
-                        <li>有专门的人辅导你</li>
-                    </ul>
-                </div>
+                <div className="inline-block text-[rgb(24,160,88)] bg-[rgba(24,160,88,0.12)] p-1">体验套餐</div>
+                <div className="text-4xl">¥9.90</div>
+                <div className="line-through text-gray-400">原价：19.90</div>
+                <ul>
+                    {items.map((item, index) =>(
+                        <li><Image
+                            src="/tick.svg"
+                            alt=""
+                            className="inline-block"
+                            width={20}
+                            height={20}
+                        />{item}</li>
+                    ))}
+                </ul>
                 <button>确认支付</button>
             </div>
-        </main>
+        </div>
     )
 }
