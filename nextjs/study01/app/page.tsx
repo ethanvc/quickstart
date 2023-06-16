58e625c687c581dc341255ed11aa35d792ca3aa9
ethanvc <xht_xht@qq.com>
2023-06-16 15:54:59 +0800

    add code

--- nextjs/study01/app/page.tsx
+++ nextjs/study01/app/page.tsx
@@ -13,21 +13,23 @@ export default function Home() {
 
 function PlanCard({items}: {items:string[]}) {
     return (<div className="h-[280px] w-[280px] border-[1px] border-[rgb(103,194,58)] p-[10px]">
-        <div className="inline-block text-[rgb(24,160,88)] bg-[rgba(24,160,88,0.12)] p-1">体验套餐</div>
-        <div className="text-4xl">¥9.90</div>
-        <div className="line-through text-gray-400">原价：19.90</div>
-        <ul>
-            {items.map((item, index) => (
-                <li><Image
-                    src="/tick.svg"
-                    alt=""
-                    className="inline-block"
-                    width={20}
-                    height={20}
-                />{item}</li>
-            ))}
-        </ul>
-        <button className="block w-[calc(100%-5px)] text-[rgb(24,160,88)] bg-[rgba(24,160,88,0.16)] p-1"
+        <div className="h-[calc(100%-32px)]">
+            <div className="inline-block text-[rgb(24,160,88)] bg-[rgba(24,160,88,0.12)] p-1">体验套餐</div>
+            <div className="text-4xl">¥9.90</div>
+            <div className="line-through text-gray-400">原价：19.90</div>
+            <ul>
+                {items.map((item, index) => (
+                    <li><Image
+                        src="/tick.svg"
+                        alt=""
+                        className="inline-block"
+                        width={20}
+                        height={20}
+                    />{item}</li>
+                ))}
+            </ul>
+        </div>
+        <button className="h-[32px] block w-[calc(100%-5px)] text-[rgb(24,160,88)] bg-[rgba(24,160,88,0.16)] p-1"
                 type="button">确认支付
         </button>
     </div>);
