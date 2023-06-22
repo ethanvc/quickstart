f8473f7c4b0894816bcc61d97269834c20784356
ethanvc <xht_xht@qq.com>
2023-06-22 08:40:41 +0800

    add code

--- main/main.go
+++ main/main.go
@@ -1,35 +1,11 @@
 package main
 
-import (
-	"fmt"
-)
+import "google.golang.org/protobuf/proto"
 
-type IAbc interface {
-	F1()
-	F2()
-}
+func Dispatch[T proto.Message](t *T) {
 
-type Abc struct {
-}
-
-func (Abc) F1() {
-	fmt.Println("Abc.F1")
-}
-
-func (Abc) F2() {
-	fmt.Println("Abc.F2")
-}
-
-type Bcd struct {
-	IAbc
 }
 
 func main() {
-	var bcd Bcd
-	var abc Abc
-	bcd.IAbc = abc
-	bcd. = func(){
-		fmt.Println("niew")
-	}
-	bcd.F2()
+
 }
