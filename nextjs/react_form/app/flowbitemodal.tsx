2d821ddde5878d46420286f45077c97717b893af
ethanvc <xht_xht@qq.com>
2023-06-29 21:11:21 +0800

    add code

--- nextjs/react_form/app/flowbitemodal.tsx
+++ nextjs/react_form/app/flowbitemodal.tsx
@@ -1,20 +1,23 @@
 import {useState} from "react";
 import { Modal, Button, Checkbox, Label, TextInput} from 'flowbite-react';
 
+let refreshCount = 0;
 export function VerifyFlowbiteModal(){
     const [formData, setFormData] =
         useState({name:"", hobby: ""});
     const onInputChange = (e)=>{
       setFormData({...formData, [e.target.name]: e.target.value});
     };
+    refreshCount++;
     return (
-        <div>
+        <div data-xx="nihao">
             <Modal show={true}>
             <Modal.Header>登录</Modal.Header>
             <Modal.Body>
                 <TextInput name="name" onChange={onInputChange}></TextInput>
                 <TextInput name="hobby" onChange={onInputChange}></TextInput>
                 <Label>{JSON.stringify(formData)}</Label>
+                <Label>{refreshCount}</Label>
             </Modal.Body>
         </Modal>
         </div>
