6c8559d6df5232fcce28b811349a0a1ea1966ef1
ethanvc <xht_xht@qq.com>
2023-07-11 21:04:06 +0800

    add code

--- goormsql_test/go.mod
+++ goormsql_test/go.mod
@@ -9,8 +9,12 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/deckarep/golang-set v0.0.0-20170826194844-b3af78e1d186 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
 	github.com/kr/pretty v0.1.0 // indirect
+	github.com/pkg/errors v0.8.1-0.20170910134614-2b3a18b5f0fb // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/schemalex/schemalex v0.1.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- goormsql_test/sqldiff_test.go
+++ goormsql_test/sqldiff_test.go
@@ -4,7 +4,10 @@ import (
 	"github.com/k0kubun/sqldef/database"
 	"github.com/k0kubun/sqldef/parser"
 	"github.com/k0kubun/sqldef/schema"
+	"github.com/schemalex/schemalex/diff"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"os"
 	"testing"
 )
 
@@ -27,4 +30,23 @@ CREATE TABLE new_table (
 		schema.GeneratorModeMysql, sqlParser, desireSql, currentSql, database.GeneratorConfig{}, "")
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(result))
+}
+
+func TestSqlDiffSchemalex(t *testing.T) {
+
+	const sql1 = `
+CREATE TABLE hoge (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id)
+);`
+	const sql2 = `
+CREATE TABLE hoge (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    c VARCHAR (20) NOT NULL DEFAULT "hoge",
+    PRIMARY KEY (id)
+);
+`
+
+	err := diff.Strings(os.Stdout, sql1, sql2)
+	require.NoError(t, err)
 }
